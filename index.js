acada2254bbac797b00e10924159e9e2ad7747a7
Rakibul-Hasan270 <rakibx270@gmail.com>
2025-04-07 20:32:46 +0600

    added some file to deploy vercel

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://car-doctor-f90b0.web.app',
+        'https://car-doctor-f90b0.firebaseapp.com'
+    ],
+    credentials: true
+}));
 app.use(express.json());
-
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ks5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+// console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -19,28 +27,56 @@ const client = new MongoClient(uri, {
         strict: true,
         deprecationErrors: true,
     }
-});
+})
+
+// middleware ...............
+const logger = async (req, res, next) => {
+    console.log(req.host, req.url)
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    if (!token) {
+        return res.status(403).send({ message: 'unauthorized' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ message: 'unauthorized' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
+const cookieOption = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+}
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const servicesCollection = client.db('carCollectionDB').collection('services');
         const bookingCollection = client.db('carCollectionDB').collection('booking');
 
         // auth related api
-        app.get('/jwt', async (req, res) => {
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-            res
-                .cookie('token', token, {
-                    httpOnly: true,
-                    secure: false,
-                    sameSite: 'none'
-                })
-                .send({ success: true })
+            res.cookie('token', token, cookieOption).send({ success: true })
         })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            // console.log('user for log out', user)
+            res
+                .clearCookie('token', { ...cookieOption, maxAge: 0 })
+                .send({ success: true })
+        })  
 
         // services related api 
         app.get('/services', async (req, res) => {
@@ -68,7 +104,12 @@ async function run() {
         })
 
         // booking api
-        app.get('/booking', async (req, res) => {
+        app.get('/booking', verifyToken, async (req, res) => {
+
+            if (req.user?.email !== req.query?.email) {
+                return res.status(401).send({ message: 'forbidden' })
+            }
+
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email };
@@ -106,7 +147,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
